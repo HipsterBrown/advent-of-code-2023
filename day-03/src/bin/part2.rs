553016bbd3b829f0e21c3560277e53a11907bcde
HipsterBrown <headhipster@hipsterbrown.com>
2023-12-04 11:15:15 -0500

    feat(day-03): complete part 2

--- day-03/src/bin/part2.rs
+++ day-03/src/bin/part2.rs
@@ -4,8 +4,140 @@ fn main() {
     dbg!(output);
 }
 
-fn part2(input: &str) -> u32 {
-    0
+#[derive(Debug, Clone, Copy)]
+struct Part {
+    col_start: usize,
+    col_end: usize,
+    row: usize,
+    value: usize,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Symbol {
+    col: usize,
+    row: usize,
+    value: char,
+}
+
+impl Symbol {
+    fn get_adjacent_parts(
+        self: &Self,
+        max_row: usize,
+        max_col: usize,
+        parts: &Vec<Part>,
+    ) -> Vec<Part> {
+        let positions = self.get_possible_positions(max_row, max_col);
+        parts
+            .iter()
+            .filter_map(|part| {
+                for position in (part.col_start..=part.col_end).map(|col| (col, part.row)) {
+                    if positions.contains(&position) {
+                        return Some(*part);
+                    }
+                }
+                None
+            })
+            .collect()
+    }
+
+    fn get_possible_positions(self: &Self, max_row: usize, max_col: usize) -> Vec<(usize, usize)> {
+        let possible_positions: Vec<(isize, isize)> = vec![
+            (1, 0),
+            (1, 1),
+            (0, 1),
+            (-1, 0),
+            (-1, -1),
+            (0, -1),
+            (1, -1),
+            (-1, 1),
+        ];
+        possible_positions
+            .iter()
+            .filter_map(|(col, row)| {
+                if row + (self.row as isize) > max_row as isize || row + (self.row as isize) < 0 {
+                    ()
+                }
+                if col + (self.col as isize) < 0 || col + (self.col as isize) > max_col as isize {
+                    ()
+                }
+                Some((
+                    (col + self.col as isize) as usize,
+                    (row + self.row as isize) as usize,
+                ))
+            })
+            .collect()
+    }
+}
+
+fn part2(input: &str) -> usize {
+    let mut parts: Vec<Part> = Vec::new();
+    let mut symbols: Vec<Symbol> = Vec::new();
+    let lines = input
+        .lines()
+        .filter(|l| !l.is_empty())
+        .collect::<Vec<&str>>();
+    let height = lines.len();
+    let width = lines[0].len();
+    for (row, line) in lines.iter().enumerate() {
+        let mut current_value = String::new();
+        for (col, c) in line.char_indices() {
+            match c {
+                '.' => {
+                    let length = current_value.len();
+                    if length > 0 {
+                        parts.push(Part {
+                            col_start: col - length,
+                            col_end: col - 1,
+                            row,
+                            value: (current_value.parse::<usize>().unwrap()),
+                        });
+                        current_value = String::new();
+                    }
+                }
+                '0'..='9' => {
+                    current_value += &c.to_string();
+                    if col == line.len() - 1 {
+                        let length = current_value.len();
+                        parts.push(Part {
+                            col_start: col - length - 1,
+                            col_end: col - 1,
+                            row,
+                            value: (current_value.parse::<usize>().unwrap()),
+                        });
+                    }
+                }
+                _ => {
+                    let length = current_value.len();
+                    if length > 0 {
+                        parts.push(Part {
+                            col_start: col - length,
+                            col_end: col - 1,
+                            row,
+                            value: (current_value.parse::<usize>().unwrap()),
+                        });
+                        current_value = String::new();
+                    }
+                    symbols.push(Symbol { col, row, value: c })
+                }
+            }
+        }
+    }
+    let gear_ratios = symbols.iter().filter_map(|symbol| {
+        if symbol.value != '*' {
+            return None;
+        }
+        let adjacent_parts = symbol.get_adjacent_parts(height, width, &parts);
+        if adjacent_parts.iter().len() < 2 {
+            return None;
+        }
+        Some(
+            adjacent_parts
+                .iter()
+                .map(|part| part.value)
+                .product::<usize>(),
+        )
+    });
+    gear_ratios.sum()
 }
 
 #[cfg(test)]
@@ -16,13 +148,18 @@ mod tests {
     fn it_works() {
         let result = part2(
             "
-Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
 ",
         );
-        assert_eq!(result, 2286);
+        assert_eq!(result, 467835);
     }
 }
